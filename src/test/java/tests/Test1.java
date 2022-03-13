9cd6e521975c369cbd77a7eeb433becf58aea86d
kalam@git143 <magnitiait@gmail.com>
2022-03-13 12:45:11 +0530

    my first commit to local repository

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class Test1 
+{
+	public static void main(String[] args)
+	{
+		String x="ufbukfhbkf#$^&&57$^784FHJ";
+		String y=x.replaceAll("[^0-9]","");
+		System.out.println(y);
+	}
+
+}
